f34d824dc4e0e695990e16cf0829d98b58203322
Adam Johnson <adam@adamjohnson.org>
2022-10-19 17:21:11 +1100

    ENV

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,6 @@ export const tokenUrl = process.env.REACT_APP_TOKEN_URL;
 export const mediaUrl = process.env.REACT_APP_MEDIA_URL;
 export const generatorUrl = process.env.REACT_APP_GENERATOR_URL;
 export const jsonRpcProviderMainnetUrl = process.env.REACT_APP_JSON_RPC_PROVIDER_MAINNET_URL || '';
-export const jsonRpcProviderRopstenUrl = process.env.REACT_APP_JSON_RPC_PROVIDER_ROPSTEN_URL || '';
 export const jsonRpcProviderGoerliUrl = process.env.REACT_APP_JSON_RPC_PROVIDER_GOERLI_URL || '';
 export const openseaBaseUrl = process.env.REACT_APP_OPENSEA_BASE_URL;
 export const etherscanBaseUrl = process.env.REACT_APP_ETHERSCAN_BASE_URL;
