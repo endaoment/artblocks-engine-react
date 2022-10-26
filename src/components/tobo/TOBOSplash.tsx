76eeb4868c295811887c8644e0a425f2edcd7026
Robbie Heeger <rheeger@gmail.com>
2022-10-25 23:36:31 -0400

    looking prettttty good for tomorrow

--- src/components/tobo/TOBOSplash.tsx
+++ src/components/tobo/TOBOSplash.tsx
@@ -2,56 +2,54 @@ import { useStickySection } from "utils/layoutEffects";
 import TOBOButton from "./TOBOButton";
 import "./toboSplash.css";
 
-/* SplashModule3 */
+/* SplashModule4 */
 
-function SplashModule3() {
-  const { ref, css } = useStickySection();
+function SplashModule4() {
+  const spaceClick = () => {
+    window.open(
+      "https://twitter.com/artblocksengine/status/1564256836764798978"
+    );
+  };
+
+  // const { ref, css } = useStickySection();
+
   return (
     <div
       className="toboSplashModule"
-      id="toboSplashModule3"
-      ref={ref}
-      style={css}
+      id="toboSplashModule4"
+      // ref={ref}
+      // style={css}
     >
       <div className="splashModuleColumn splashModuleColumnL">
-        <img src="img/tobo/splash/module-3-hero.png" alt="" />
+        <h3>FROM ARTISTS WHO UNDERSTAND</h3>
+        <p>
+          "Some people might navigate workload and pressure more efficiently
+          than others, but once the breaking point is reached, we all end up in
+          the same place.
+        </p>
+        <p>
+          We're happy to support initiatives that acknowledge caring individuals
+          that are subject to experiencing burnout.”
+        </p>
+        <p>
+          -&nbsp;
+          <a
+            href="https://twitter.com/ArtOnBlockchain?s=20&t=1a6ek0BPuap8dwKJtBGhuQ"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Erick Calderon, Arblocks CEO
+          </a>
+        </p>
       </div>
 
       <div className="splashModuleColumn splashModuleColumnR">
-        <h3>SHARE THE CAUSE</h3>
-        <p>
-          Endaoment and Sostento are 501c3 charitable organizations. Proceeds
-          will directly support programs that reduce burnout among frontline
-          healthcare workers who provide services to families living in poverty.
-        </p>
-
-        <div id="splashModule3Social">
-          <span>SHARE THE CAUSE</span>
-          <a
-            href="https://twitter.com/endaomentdotorg"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Twitter
-          </a>
-          <a
-            href="https://www.linkedin.com/company/endaoment/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            LinkedIn
-          </a>
-          <a
-            href="https://www.instagram.com/endaomentdotorg/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Instagram
-          </a>
-          <a href="https://vimeo.com/" target="_blank" rel="noreferrer">
-            Vimeo
-          </a>
-        </div>
+        <img
+          src="https://endaoment-gallery-for-good---flex-goerli.s3.amazonaws.com/8.png"
+          alt=""
+        />
+        <div className="splashModule2Circle" id="splashModule2Circle1"></div>
+        <div className="splashModule2Circle" id="splashModule2Circle2"></div>
       </div>
 
       <div className="clear"></div>
@@ -59,33 +57,91 @@ function SplashModule3() {
   );
 }
 
+/* SplashModule3 */
+
+function SplashModule3() {
+  const spaceClick = () => {
+    window.open(
+      "http://twitter.com/intent/tweet?text=Join%20me%20November%2015th%20for%20the%20%23TurnoutForBurnout!%20Mint%20generative%20art%20to%20fund%20wellness%20programs%20for%20healthcare%20workers.%20%0A%0ALearn%20more%20at%3A&url=http%3A%2F%2Fwww.turnoutforburnout.com%0A%0ABrought%20to%20you%20by%20your%20friends%20at%0A%40EndaomentDotOrg%20%40Sostento%20and%20%40Artblocks_io%20"
+    );
+  };
+
+  // const { ref, css } = useStickySection();
+  return (
+    <div
+      className="toboSplashModule"
+      id="toboSplashModule3"
+      // ref={ref}
+      // style={css}
+    >
+      <div className="splashModuleColumn splashModuleColumnL">
+        <img src="img/tobo/splash/module-3-hero.png" alt="" />
+      </div>
+
+      <div className="splashModuleColumn splashModuleColumnR">
+        <h3>FIGHTING BURNOUT</h3>
+        <p>
+          <a href="https://sostento.org" target="_blank" rel="noreferrer">
+            Sostento
+          </a>{" "}
+          is a tax-exempt 501c3 charitable organization. Proceeds will directly
+          support programs that reduce burnout among frontline healthcare
+          workers who provide services to families living in poverty.
+        </p>
+
+        <div id="splashModule3Social">
+          <span>TWEET YOUR SUPPORT</span>
+          <TOBOButton text="Post Now" action={spaceClick} />
+        </div>
+      </div>
+
+      <div className="clear"></div>
+    </div>
+  );
+}
 /* SplashModule2 */
 
 function SplashModule2() {
   const spaceClick = () => {
-    window.open("https://endaoment.org");
+    window.open(
+      "https://twitter.com/artblocksengine/status/1564256836764798978"
+    );
   };
 
-  const { ref, css } = useStickySection();
+  // const { ref, css } = useStickySection();
 
   return (
     <div
       className="toboSplashModule"
       id="toboSplashModule2"
-      ref={ref}
-      style={css}
+      // ref={ref}
+      // style={css}
     >
       <div className="splashModuleColumn splashModuleColumnL">
-        <h3>THE COLORS THAT HEAL</h3>
+        <h3>"THE COLORS THAT HEAL"</h3>
         <p>
-          This generative art collection is a box of memories,a garden of
-          hope,and a beacon of encouragement via ArtBlocks Engine platform.
+          Our marquee generative art collection is a box of memories,a garden of
+          hope,and a beacon of encouragement from generative artist Ryan Green.
         </p>
-        <TOBOButton text="Come to the Space" action={spaceClick} />
+        <TOBOButton text="About AB Engine" action={spaceClick} />
+        <p>
+          "Having someone that will say 'I see you, let me go with you' is a
+          simple gesture that is profoundly encouraging.” -&nbsp;
+          <a
+            href="https://twitter.com/ryangreen8"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Ryan Green
+          </a>
+        </p>
       </div>
 
       <div className="splashModuleColumn splashModuleColumnR">
-        <img src="https://res.cloudinary.com/art-blocks/image/fetch/f_auto,c_limit,w_384,q_auto/https://endaoment-gallery-for-good---flex-goerli.s3.amazonaws.com/20.png" alt="" />
+        <img
+          src="https://endaoment-gallery-for-good---flex-goerli.s3.amazonaws.com/20.png"
+          alt=""
+        />
         <div className="splashModule2Circle" id="splashModule2Circle1"></div>
         <div className="splashModule2Circle" id="splashModule2Circle2"></div>
       </div>
@@ -99,21 +155,21 @@ function SplashModule2() {
 
 function SplashModule1() {
   const nftClick = () => {
-    window.open("https://endaoment.org");
+    window.open("https://opensea.io/endaoment");
   };
 
   const cryptoClick = () => {
-    window.open("https://endaoment.org");
+    window.open("https://app.endaoment.org/orgs/843739888");
   };
 
-  const { ref, css } = useStickySection();
+  // const { ref, css } = useStickySection();
 
   return (
     <div
       className="toboSplashModule"
       id="toboSplashModule1"
-      ref={ref}
-      style={css}
+      // ref={ref}
+      // style={css}
     >
       <img
         src="img/tobo/splash/module-1-shape.png"
@@ -133,10 +189,11 @@ function SplashModule1() {
           id="splashModule1Logo"
         />
         <p>
-          Raising funds to reduce burnout among hard working health workers
-          through an NFT charity mining event &amp; auction.
+          Face it—we're all a bit burnt out. Let's raise funds to reduce burnout
+          amongst healthcare workers through a generative art event &amp;
+          auction.
         </p>
-        <TOBOButton text="Donate NFT" viaEndaoment={true} action={nftClick} />
+        <TOBOButton text="Donate NFTs" viaEndaoment={true} action={nftClick} />
         <TOBOButton
           text="Donate Crypto"
           viaEndaoment={true}
@@ -145,13 +202,13 @@ function SplashModule1() {
 
         <div id="splashModule1Mint">
           <TOBOButton text="Mint a Charity Piece" disabled={true} />
-          <span>Minting November X, 2022</span>
+          <span>Minting begins November 15th, 2022</span>
         </div>
       </div>
 
       <div className="clear"></div>
       <div id="splashModule1Banner">
-        Art Blocks Mint + <span>&nbsp;Charity Fundraiser</span>
+        Generative Art + <span>&nbsp;Charity Fundraiser</span>
       </div>
     </div>
   );
@@ -174,17 +231,16 @@ function SplashHeader() {
         <br />
         Collective Action
       </h2>
-
       <div id="splashHeaderLogos">
         <span>POWERED BY</span>
-        <a href="https://www.artblocks.io/" target="_blank" rel="noreferrer">
-          <img src="img/tobo/logo-artblocks.png" alt="ArtBlocks" />
-        </a>
         <a href="https://sostento.org/" target="_blank" rel="noreferrer">
           <img src="img/tobo/logo-sostento.png" alt="Sostento" />
         </a>
         <a href="https://endaoment.org/" target="_blank" rel="noreferrer">
           <img src="img/tobo/logo-endaoment.png" alt="Endaoment" />
+        </a>
+        <a href="https://www.artblocks.io/" target="_blank" rel="noreferrer">
+          <img src="img/tobo/logo-artblocks.png" alt="ArtBlocks" />
         </a>
       </div>
     </div>
@@ -200,6 +256,7 @@ const TOBOSplash = () => {
       <SplashModule1 />
       <SplashModule2 />
       <SplashModule3 />
+      <SplashModule4 />
     </div>
   );
 };
