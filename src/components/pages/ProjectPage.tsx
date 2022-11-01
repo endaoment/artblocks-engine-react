f3e9f424e1d8b00beb38fc4dc869a99ee0ce5dac
Noah Gallant <gallantno@gmail.com>
2022-11-01 16:54:50 -0400

    refactor: project list on project page

--- src/components/pages/ProjectPage.tsx
+++ src/components/pages/ProjectPage.tsx
@@ -1,25 +1,25 @@
-import Page from 'components/Page';
-import Alert from '@mui/material/Alert';
-import ProjectDetails from 'components/ProjectDetails';
-import { useParams } from 'react-router-dom';
-import { coreContractAddress } from 'config';
+import Page from "components/Page";
+import Alert from "@mui/material/Alert";
+import ProjectDetails from "components/ProjectDetails";
+import { useParams } from "react-router-dom";
+import { coreContractAddress } from "config";
+import ProjectList from "components/ProjectList";
 
 const ProjectPage = () => {
   const { projectId } = useParams();
 
   return (
     <Page>
-      {
-        projectId ? (
-          <ProjectDetails id={coreContractAddress?.toLowerCase() + '-' + projectId} />
-        ) : (
-          <Alert severity="info">
-            Project not found
-          </Alert>
-        )
-      }
+      {projectId ? (
+        <ProjectDetails
+          id={coreContractAddress?.toLowerCase() + "-" + projectId}
+        />
+      ) : (
+        <Alert severity="info">Project not found</Alert>
+      )}
+      <ProjectList />
     </Page>
-  )
-}
+  );
+};
 
 export default ProjectPage;
