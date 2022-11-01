0e5522a29a0b030513eeda9ca5e2fbe185ad5091
Adam Johnson <adam@adamjohnson.org>
2022-11-01 21:11:30 +1100

    New header

--- src/components/tobo/TOBOHeader.tsx
+++ src/components/tobo/TOBOHeader.tsx
@@ -1,3 +1,4 @@
+import TOBOConnectWallet from './TOBOConnectWallet';
 import './toboHeader.css';
 
 /* TOBOHeader */
@@ -7,7 +8,10 @@ const TOBOHeader = () => {
     <header>
       <h1>
         <img src="img/tobo/logo.svg" alt="TURNOUT FOR BURNOUT" id="toboLogo" />
+        <img src="img/tobo/logo-footer.svg" alt="TURNOUT FOR BURNOUT" id="toboMobileLogo" />
       </h1>
+
+      <TOBOConnectWallet />
     </header>
   );
 }
