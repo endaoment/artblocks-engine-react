2d2cd5b8d32f21fe9ef76170752aefc67446c25a
Robbie Heeger <rheeger@gmail.com>
2022-11-15 10:42:46 -0500

    add ryans address to button

--- src/components/PurchaseProject.tsx
+++ src/components/PurchaseProject.tsx
@@ -85,7 +85,10 @@ const PurchaseProject = ({ project }: Props) => {
     return null;
   }
 
-  if (project.paused) {
+  if (
+    project.paused &&
+    account !== "0x8bB1A6245603a30eb5B3Bf51c369089927979A5F"
+  ) {
     return (
       <Button variant="contained" color="primary" disabled>
         Purchases paused
